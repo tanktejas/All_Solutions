8b0101152def3e641dbd7b77b0267c154ac35880
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-04-21 19:23:43 +0530

    Time: 408 ms (17.92%), Space: 40.2 MB (86.82%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,22 +1,79 @@
+class node{
+    node* next;
+    int val;
+    public:
+     node(){}
+     node(int data){
+         next=NULL;
+         val=data;
+     }    
+    node* add(node* root,int val){
+        if(!root){
+            return root=new node(val);
+        }
+        node* temp=root;
+      
+          while(temp->next){
+            temp=temp->next;
+          } 
+           
+          temp->next=new node(val);
+       
+        return root;
+    }
+   node* remove(node* root,int val){
+       
+        
+       while(root and root->val==val) root=root->next;
+       
+       node* temp=root;
+       
+       
+       while(temp and temp->next){
+           if(temp->next->val==val){
+               node* curr=temp;
+               temp=temp->next;
+               while(temp and temp->val==val){
+                   temp=temp->next;
+               } 
+               curr->next=temp; 
+           }else{
+               temp=temp->next;
+           }
+       }
+       
+    return root;
+   }
+  bool contain(node* root,int val){
+      node* temp=root;
+      while(temp){
+          if(temp->val==val) return 1;
+        temp=temp->next;
+      }
+      return 0;
+  }
+};
 class MyHashSet {
 public:
-    vector<int>v;
+    node* root;
+    node obj;
     MyHashSet() { 
-        v.resize(1e6+1,0);
+        root=NULL;
     }
     
     void add(int key) {
-        v[key]=1;
-    }
+       root=obj.add(root,key);
+    }     
     
     void remove(int key) {
-        v[key]=0;
+        root=obj.remove(root,key);
     }
     
     bool contains(int key) {
-        return v[key];
+        return obj.contain(root,key);
     }
-};
+}; 
+
 
 /**
  * Your MyHashSet object will be instantiated and called as such:
