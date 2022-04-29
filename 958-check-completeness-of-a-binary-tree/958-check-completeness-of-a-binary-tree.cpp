e273091585da8ac95b0585972fd54bd7f1b53868
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-04-29 14:15:01 +0530

    Time: 3 ms (90.38%), Space: 10.3 MB (92.84%) - LeetHub

--- 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.cpp
+++ 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.cpp
@@ -11,38 +11,35 @@
  */
 class Solution {
 public:
-    bool dolevel(TreeNode* root){
-        queue<TreeNode*>q;
-        bool isnull=0;
-        q.push(root);
-        q.push(NULL);
-        while(q.size()>1){
-            if(q.front()==NULL){
-                q.pop();
-                q.push(NULL);
-            }
-            TreeNode* curr=q.front();
-            q.pop();
-            if(curr->left==NULL){
-                isnull=1;
+    bool dodfs(TreeNode* root,int &height,int curr,bool &lessfnd){
+        if(!root){ 
+            cout<<curr<<" ";
+            if(height==-1){
+                height=curr;
+                return 1;
             }else{
-                if(isnull) return 0;
-                else{
-                    q.push(curr->left);
-                }
+                 if(lessfnd){ 
+                     if((curr+1)==(height)) return 1;
+                     else return 0;
+                 }else{
+                     if(height==curr) return 1;  
+                     else if(height == (curr+1)){
+                         lessfnd=1;
+                         return 1;
+                     }else{
+                         return 0;
+                     }
+                 }
             }
-            if(curr->right==NULL){
-                isnull=1;
-            }else{
-                if(isnull) return 0;
-                else{
-                    q.push(curr->right);
-                }
+        } 
+        if(!dodfs(root->left,height,curr+1,lessfnd) or !dodfs(root->right,height,curr+1,lessfnd)){
+                return 0;
             }
-        }
         return 1;
     } 
     bool isCompleteTree(TreeNode* root) {
-       return dolevel(root);    
+        int height=-1;
+        bool lessfnd=0;     
+       return dodfs(root,height,0,lessfnd);    
     }
 };
