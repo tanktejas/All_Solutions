82f4a492576763db2fca94c373879bb08f606d07
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-05-28 10:45:38 +0530

    Time: 29 ms (43.05%), Space: 18.1 MB (20.98%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -5,15 +5,14 @@ public:
         nums.resize(n+1);
         nums[n]=-1;
         for(int i=0;i<n+1;i++){
-             if(nums[i]!=i){
-                 // swap(nums[i],nums[nums[i]]);
+             if(nums[i]!=i){ 
                  while(i!=-1 and nums[i]!=-1 and i!=nums[i]){
                      swap(nums[i],nums[nums[i]]);
                  }
              }
-        }
-        for(int i=0;i<n+1;i++) cout<<nums[i]<<" ";
-        cout<<endl;
+        } 
+        
+        
         for(int i=0;i<n+1;i++){
             if(nums[i]!=i){
                 return i;
