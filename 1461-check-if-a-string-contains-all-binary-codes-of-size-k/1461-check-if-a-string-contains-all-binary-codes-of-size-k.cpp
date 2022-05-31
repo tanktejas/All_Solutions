3fd4a6d697603d215b95c29fb7e3393cec53c4d3
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-05-31 18:58:50 +0530

    Time: 129 ms (88.05%), Space: 58.4 MB (62.58%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -4,22 +4,22 @@ public:
         if(s.length()<=k){
             return 0;
         }
+        
         int ss=pow(2,k);
         vector<int>mark(ss,0);
         int allones=0;
         for(int i=0;i<k;i++){
             allones=(allones)|(1<<i);
-        }
-        // cout<<allones; 
-        int wind=stoi(s.substr(0,k),0,2);
-        cout<<wind<<" ";
+        } 
+        
+        int wind=stoi(s.substr(0,k),0,2); 
+        
         int n=s.length();
         int cnt=1;
         mark[wind]=1;
         for(int i=k;i<n;i++){
             wind=(wind<<1)&(allones);
-            wind|=((s[i]-'0'));
-            // cout<<wind<<" ";
+            wind|=((s[i]-'0')); 
             if(mark[wind]==0){
                 mark[wind]=1;
                 cnt++;
