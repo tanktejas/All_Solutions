5238dd5ddb36be937d499b869da758a7980d392d
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-06-02 10:21:43 +0530

    Time: 15 ms (65.95%), Space: 10.6 MB (43.03%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -4,6 +4,7 @@ public:
         int n=mat.size();
         int m=mat[0].size();
         vector<vector<int>>mat1(m,vector<int>(n));
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 mat1[j][i]=mat[i][j]; 
