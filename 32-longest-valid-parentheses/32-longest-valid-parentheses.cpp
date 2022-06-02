99fe5b03bf83de356d549e156d6add1d9c44dc85
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-06-02 15:39:40 +0530

    Time: 9 ms (29.59%), Space: 7.6 MB (10.93%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,53 +1,29 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
+    while(s.size() and s[0]==')') s=s.substr(1);
         
-        int ans=0;
-        int n=s.length();
-        int lef=-1;
-        int op=0,cl=0;
-        for(int i=0;i<n;i++){
-            if(s[i]=='('){
-                 op++;
+      stack<pair<char,int>>st;
+        st.push({'(',-1});
+      int mx=0;
+      int n=s.length();
+    
+      for(int i=0;i<n;i++){
+          
+        if(s[i]=='('){
+            st.push({'(',i});
+        }else{   
+            if(st.top().first=='(' and st.top().second!=-1){
+                   st.pop();  
+                   mx=max(mx,i-st.top().second); 
             }else{
-                cl++;
+                st.push({')',i});
             }
-            
-            if(op==cl){
-                ans=max(ans,(i-lef));
-            }
-            
-            if(cl>op){
-                op=0;
-                cl=0;
-                lef=i;
-            }
-            
-        } 
+         }
+          
+      }   
         
-        op=0;
-        cl=0;
-        lef=n;
-        
-         for(int i=n-1;i>=0;i--){
-            if(s[i]=='('){
-                 op++; 
-            }else{
-                cl++;
-            }
-            
-            if(op==cl){
-                ans=max(ans,(lef-i));
-            }
-            
-            if(cl<op){
-                op=0;
-                cl=0;
-                lef=i;
-            }
-            
-        } 
-        
-        return ans;
+       return mx;
     }
 };
+
