feb154cef492d45b65b31b8fa0f243c4df702c61
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-06-13 11:07:10 +0530

    Time: 597 ms (81.97%), Space: 129.2 MB (82.94%) - LeetHub

---
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& op) {
+         unordered_map<int,int>m;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+             m[nums[i]]=i;
+        }
+        
+        for(int i=0;i<op.size();i++){
+            int find=op[i][0];
+            int idx=m[find];
+            m[op[i][1]]=idx;
+            nums[idx]=op[i][1];
+        }
+        
+        return nums;
+    }
+};
