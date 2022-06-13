7b9f9176ff3e72293384b341e91b572d4c84594b
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-06-13 11:09:37 +0530

    Time: 491 ms (95.09%), Space: 129.3 MB (82.94%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
@@ -10,7 +10,7 @@ public:
         for(int i=0;i<op.size();i++){
             int find=op[i][0];
             int idx=m[find];
-            m[op[i][0]]=-1;
+            m[op[i][0]]=-1;  // we have to write this if (operations[i][0] not exists in nums.) because in this case we may found some index for non existing element which we previously swapped so need to update.
             m[op[i][1]]=idx;
             nums[idx]=op[i][1];
         }
