80ab9629f09cce1630d362af52bdc4c0653f9bfc
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-06-19 18:22:23 +0530

    Time: 148 ms (56.42%), Space: 83 MB (39.75%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,22 +1,82 @@
 class Solution {
 public:
+    class trienode{
+        public:
+           trienode* v[26];
+           vector<int>idx;
+          
+    };
+    
+    
+    class trie{
+        trienode* node1;
+        public:
+          trie(){
+            node1=new trienode();
+            for(int i=0;i<26;i++){
+                node1->v[i]=NULL;
+            }
+          }
+         
+        void insert(string s,int ind){
+            trienode* node=node1;
+            for(int i=0;i<s.length();i++){
+                
+                if(node->v[s[i]-'a']==NULL){
+                    node->v[s[i]-'a']=new trienode();
+                    node=node->v[s[i]-'a'];
+                }else{
+                    node=node->v[s[i]-'a'];   
+                }
+              
+                node->idx.push_back(ind);
+            }
+        }
+        
+        vector<int> search(string pre){
+             trienode* node=node1;
+            for(int i=0;i<pre.length();i++)
+            {
+                if(node->v[pre[i]-'a']==NULL){
+                    return {};
+                }else{
+                    node=node->v[pre[i]-'a'];
+                }
+            }
+            
+             return node->idx;
+        }
+        
+    };
+    
     vector<vector<string>> suggestedProducts(vector<string>& pro, string sw) {
        sort(pro.begin(),pro.end());
         vector<vector<string>>ans;
         
-        for(int i=1;i<=sw.size();i++){
-            vector<string>temp;
-            int cnt=1;
-            string cc=sw.substr(0,i);
-            for(int j=0;j<pro.size() and cnt<=3;j++){
-                 string ff=pro[j].substr(0,i);
-                 if(cc==ff)
-                 {
-                     temp.push_back(pro[j]);
-                     cnt++;
-                 }
+        trie t;
+        
+        for(int i=0;i<pro.size();i++){
+              t.insert(pro[i],i);
+        }
+        string tt="";
+        
+        for(int i=0;i<sw.length();i++){
+             tt+=sw[i];
+            
+            vector<int> bb=t.search(tt);
+            if(bb.size()>3){
+                vector<string>h(3);
+                h[0]=pro[bb[0]];  
+                h[1]=pro[bb[1]];
+                h[2]=pro[bb[2]];
+                ans.push_back(h);
+            }else{  
+                vector<string>h;
+                for(int i=0;i<bb.size();i++){
+                    h.push_back(pro[bb[i]]);
+                }
+                ans.push_back(h);
             }
-            ans.push_back(temp);
         }
         
         return ans;
