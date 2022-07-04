ce3b67be7dc625c10eaebd0ffe5335390ac0a2c3
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-07-04 12:04:13 +0530

    Time: 189 ms (5.09%), Space: 20.4 MB (5.02%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int candy(vector<int>& r) {
+        int n=r.size();
+        
+        if(n==1) return 1;
+        
+        vector<int>v(n,0);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+        
+        for(int i=0;i<n;i++){
+             q.push({r[i],i});
+        }
+        
+       
+        
+        while(!q.empty()){
+            auto ele=q.top();
+            q.pop();
+            if(ele.second==0){
+                if(r[ele.second+1] < ele.first){
+                    v[ele.second]=v[ele.second+1]+1;
+                }else{
+                    v[ele.second]=1;
+                } 
+            }else if(ele.second==n-1){
+                if(r[ele.second-1] < ele.first){
+                    v[ele.second]=v[ele.second-1]+1;
+                }else{
+                    v[ele.second]=1;
+                }
+            }else{
+                int mm=0;
+                if(r[ele.second-1] < ele.first)
+                  mm=max(mm,v[ele.second-1]);
+                if(r[ele.second+1] < ele.first)
+                 mm=max(mm,v[ele.second+1]);
+                 
+                 
+                    v[ele.second]=1+mm; 
+            
+            }
+        }   
+        
+        int s=0;
+        for(int i=0;i<n;i++){
+            s+=v[i];
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
+        return s;
+    }
+};
