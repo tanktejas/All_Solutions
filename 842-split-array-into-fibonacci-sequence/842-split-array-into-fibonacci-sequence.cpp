6cc07b9c582226761ecc1df82e2a2cd64414db03
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-07-06 23:45:40 +0530

    Time: 104 ms (5.24%), Space: 12.5 MB (7.33%) - LeetHub

---
+++ 842-split-array-into-fibonacci-sequence/842-split-array-into-fibonacci-sequence.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int>ans; 
+     
+    
+    bool find(int idx,vector<int> &tmp,string str){
+        if(tmp.size()>=3){ 
+            int n=tmp.size()-1; 
+            long long pp= tmp[n-1];
+            long long p2= tmp[n-2];
+            long long oo=(long long)(pp+p2);
+            if(tmp[n]!=oo) return 0;
+        } 
+        
+        if(idx==str.length()){
+            if(tmp.size()>2){
+              ans=tmp;
+              // cout<<idx<<" "<<tmp.size()<<endl;
+              return 1;
+           }
+            return 0;  
+        }
+        
+        if(str[idx]=='0'){
+            tmp.push_back(0);
+            if(find(idx+1,tmp,str)) return 1;
+            tmp.pop_back();
+            return 0;
+        }
+        
+        for(int i=idx;(i-idx+1)<=10 and i<str.length() ;i++){  
+            
+                   string chosen=str.substr(idx,(i-idx+1)); 
+                   long long rr=stoll(chosen); 
+                   cout<<chosen<<" "<<rr<<' '<<endl;  
+                   tmp.push_back(rr);
+                   if(find(i+1,tmp,str)){
+                         return 1;
+                   }  
+                  tmp.pop_back();
+            }
+        return 0;
+    
+    }
+          
+    vector<int> splitIntoFibonacci(string num) {
+         vector<int>tmp;
+         find(0,tmp,num);
+         return ans;
+    }
+};
