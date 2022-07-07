67ff56bae30861fd73d3b8cf3df5cd2cda3ea506
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-07-07 22:11:26 +0530

    Time: 6 ms (63.84%), Space: 6.4 MB (77.07%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,3 +1,47 @@
+// class Solution {
+// public:
+//     string f,s,t;
+//     int dp[101]; 
+        
+//     bool find(int i,int j,int k){
+//        if(k==t.length()) return 1;
+        
+//        if(i==f.length()) return s.substr(j)==t.substr(k);
+//        if(j==f.length()) return f.substr(i)==t.substr(k);
+        
+//         if(dp[j]!=-1) return 0;
+        
+//        if(s[j]!=t[k] and f[i]!=t[k]){
+//            dp[j]=0;  
+//            return 0;
+//        } 
+        
+//        if(s[j]==t[k] and f[i]!=t[k]){
+//            dp[j]=find(i,j+1,k+1);
+//        }
+//        else if(f[i]==t[k] and s[j]!=t[k]){
+//            dp[j]=find(i+1,j,k+1);
+//        }else{
+//            int pick=find(i+1,j,k+1) or find(i,j+1,k+1);
+//            dp[j]=pick;
+//        }
+        
+//         return dp[j];
+//     }
+    
+//     bool isInterleave(string s1, string s2, string s3) {
+//         if((s1.length()+s2.length())!=s3.length()) return 0;
+//         memset(dp,-1,sizeof(dp));
+        
+//         f=s1;
+//         s=s2;
+//         t=s3;
+         
+//        return find(0,0,0);
+//     }
+// };
+
+ 
 class Solution {
 public:
     string f,s,t;
@@ -39,5 +83,3 @@ public:
        return find(0,0,0);
     }
 };
-
- 
