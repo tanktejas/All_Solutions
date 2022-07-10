915435f7191bcc8319ab2890de7ec5163b8d373d
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-07-10 10:37:03 +0530

    Time: 10 ms (58.03%), Space: 13.6 MB (96.37%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        vector<int>dp(n,0);
+        dp[n-1]=cost[n-1];
+        dp[n-2]=cost[n-2];
+        
+        for(int i=n-3;i>=0;i--){
+            dp[i]=cost[i] + min(dp[i+1],dp[i+2]);
+        }
+        
+        return min(dp[0],dp[1]);
+    }
+};
