ab77e264f4f029b06d0061104af4bbdea9a305e1
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-07-22 12:07:01 +0530

    Time: 218 ms (5.01%), Space: 20 MB (8.05%) - LeetHub

---
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    unordered_map<string,vector<string>>adj;
+    map<pair<string,string>,int>vis;
+    vector<string>ans,ans1;
+    
+    bool dfs(string so,int &n)
+    {  
+        
+        if(ans.size()==n+1) 
+        {   
+            if(ans1.size()==0){
+                ans1=ans;      
+            } 
+            return 1;
+        }
+        
+        for(auto ele:adj[so])
+        {
+            if(vis.find({so,ele})!=vis.end())
+            {
+                     vis[{so,ele}]--;
+                     if(vis[{so,ele}]==0) vis.erase({so,ele});
+                     ans.push_back(ele);
+                     if(dfs(ele,n)) return 1;
+                     ans.pop_back(); 
+                     vis[{so,ele}]++; 
+            }
+        }
+        return 0;
+    }
+    
+    vector<string> findItinerary(vector<vector<string>>& ti) {
+        int n=ti.size();
+        for(int i=0;i<n;i++)
+        {
+            string s=ti[i][0];
+            string d=ti[i][1];
+            
+            vis[{s,d}]++;
+            
+            adj[s].push_back(d);     
+        }
+        
+       for(auto &vele:adj)
+       {
+           sort(vele.second.begin(),vele.second.end());
+       }
+       
+        ans.push_back("JFK");
+        
+        dfs("JFK",n);
+        
+        return ans1;
+        
+    }
+};
