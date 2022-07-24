7b7f6ec648411e4344f6d0dfb55b1aac29bf47a8
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-07-24 12:02:25 +0530

    Time: 707 ms (50.00%), Space: 99.5 MB (50.00%) - LeetHub

---
+++ 2354-number-of-excellent-pairs/2354-number-of-excellent-pairs.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public: 
+    long long countExcellentPairs(vector<int>& nums, int k) { 
+        vector<long long>stbit(74,0);
+        
+        sort(nums.begin(),nums.end());
+        map<int,int>m;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            
+         if(m[nums[i]]==0){   
+          int sb=__builtin_popcount(nums[i]);
+          stbit[sb]++;
+          m[nums[i]]=1;
+         }
+        }
+        
+        long long cnt=0;
+        
+        
+        // for(int i=0;i<n;i++) cout<<stbit[i]<<" ";
+        
+        for(int i=0;i<=70;i++)
+        {
+            
+            for(int j=0;j<=70;j++)
+            {
+                if((i+j) >= k)
+                {
+                    cnt+=(1LL * stbit[i] * stbit[j]);
+                }
+            }
+            
+            
+        }
+        
+        
+        return cnt;
+    }
+};  
