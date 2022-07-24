7cf16dc10a4c1a7af2583c698c9d221340ca6038
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-07-24 14:22:49 +0530

    Time: 3 ms (88.27%), Space: 11.2 MB (29.04%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int i=0,j=nums.size()-1;
+        
+        while(i<=j)
+        {
+            int mid=(i+j)/2;
+            
+            if(nums[mid]==target) return mid;
+            
+            if(nums[mid] >= nums[i]){
+                
+                if(target >= nums[i] and target<nums[mid])
+                {
+                    j=mid-1;
+                }else{
+                    i=mid+1;
+                }
+                
+            }else{
+                
+                if(target >= nums[mid] and target <= nums[j])
+                    i=mid+1;
+                else 
+                    j=mid-1;
+                
+            }
+        }
+        
+        return -1;
+    }
+};
