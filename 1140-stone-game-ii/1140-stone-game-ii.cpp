3944bf002e5e3ac506219c203cad9dab4c29ab13
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-08-03 00:10:37 +0530

    Time: 36 ms (50.44%), Space: 14.3 MB (18.58%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -5,19 +5,30 @@ public:
     
     int find(int n,int idx,int m)
     {
-         if(idx>pre.size()) return 0;
+         if(idx>=(n+1)) return 0;
         
-         if(dp[idx][m]!=-1) return dp[idx][m];
-        
+         if(dp[idx][m]!=INT_MIN) return dp[idx][m];
+         
           int ele=INT_MIN;
-          int tot=0;
         
-          for(int i=0;i<2*m;i++)
+          for(int i=idx;i<=((idx-1)+2*m);i++)
           { 
-              if((i+idx)<pre.size()) tot+=pre[idx+i]; 
-                 int elemax=tot - find(n,idx+i+1,max(m,i+1)); 
-                 ele=max(ele,elemax);  
-          }
+              // if(i<n){   
+                
+                 int picked;
+                  if(i<=n)  
+                   picked=pre[i]-pre[idx-1];
+                 int uu=max(m,(i-idx+1));  
+                 int yy=find(n,i+1,uu); 
+                if(yy!=INT_MIN and i<=n){
+                 
+                    int elemax=picked - yy;
+                    ele=max(ele,elemax);  
+                  
+                }
+                 
+              // }
+          }   
          
         return dp[idx][m]=ele;
         
@@ -25,24 +36,24 @@ public:
     
     int stoneGameII(vector<int>& piles) {
         int n=piles.size();
-        dp.resize(104,vector<int>(104,-1));
-        pre=piles;
+        dp.resize(104,vector<int>(104,INT_MIN));
+        pre.resize(n+3,0);
         
-//         pre[1]=piles[0]; 
+        pre[1]=piles[0]; 
         int sum=0;
         
-//         for(int i=1;i<n;i++)
-//         {  
-//             pre[i+1]=pre[i]+piles[i];
+        for(int i=1;i<n;i++)
+        {  
+            pre[i+1]=pre[i]+piles[i];
         
-//         }
+        }
         
         for(int i=0;i<piles.size();i++)
         {
             sum+=piles[i];
         }
         
-        return (sum+find(n,0,1))/2;
+        return (sum+find(n,1,1))/2;
         
     }
 };
