8928230dba539e771cb3276c803458fb1cfa0c32
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-08-16 11:56:09 +0530

    Time: 647 ms (50.54%), Space: 8.6 MB (66.31%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int beautySum(string &s) {
+        int n=s.length(); 
+        int ans=0;
+        
+        for(int i=0;i<n;i++)
+        {
+             vector<int>pre(26,0); 
+            
+            for(int j=i;j<n;j++)
+            {
+                
+                 int mini=INT_MAX,maxi=INT_MIN;
+                  pre[s[j]-'a']++;
+                
+                 for(int k=0;k<26;k++)
+                 {
+                     if(pre[k]==0) continue;
+                      mini=min(mini,pre[k]);   
+                      maxi=max(maxi,pre[k]);  
+                 }   
+                
+               ans+=(maxi-mini);    
+           }     
+        }     
+         
+        return ans;
+    }
+};
