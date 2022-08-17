8df7cd396298aa7c30d3b1b4fd46d15bbc62ba12
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-08-17 15:44:25 +0530

    Time: 727 ms (6.04%), Space: 85.6 MB (36.15%) - LeetHub

---
+++ 164-maximum-gap/164-maximum-gap.cpp
@@ -0,0 +1,86 @@
+class Solution {
+public:
+   
+    vector<int>temp;
+    
+    void docount(int last,vector<int> &nums)
+    { 
+        
+         int dev=1; 
+         
+         while(last>0){
+             dev*=10;
+             last--;
+         }
+ 
+        
+         vector<int>v(10,0);
+         int n=nums.size();
+         
+         temp.resize(n,0);
+          
+         for(int i=0;i<n;i++)
+         {  
+             int ele=(nums[i]/dev)%10;
+             v[ele]++;   
+         }
+          
+         for(int i=1;i<10;i++)
+         {
+             v[i]+=v[i-1];
+         } 
+        
+           
+         for(int i=n-1;i>=0;i--)
+         { 
+             int ele=(nums[i]/dev)%10; 
+             int idx=v[ele]-1;    
+             v[ele]--;   
+             temp[idx]=nums[i];
+         }
+          
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=temp[i];
+        }
+          
+    }
+     
+    int maximumGap(vector<int>& nums) {
+         
+        int n=nums.size();
+        
+        if(n<2) return 0;
+        
+        int maxi;
+        for(int i=0;i<n;i++)
+        {
+            maxi=max(maxi,nums[i]);
+        }
+        
+        int tillwhen=0;
+        while(maxi)
+        {
+            tillwhen++;
+            maxi/=10;
+        }  
+        int curr=0;  
+        
+        while(curr<tillwhen)
+        {  
+            docount(curr,nums);  
+            curr++; 
+        }
+         
+        int diff=nums[1]-nums[0];
+        
+        for(int i=2;i<n;i++)
+        {
+             diff=max(diff,nums[i]-nums[i-1]);
+        }
+       
+        cout<<endl;  
+        
+        return diff;
+    }
+};
