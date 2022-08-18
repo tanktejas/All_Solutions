8d7731230965e2b45eeba375b51bd25b5159d163
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-08-18 10:09:49 +0530

    Time: 3 ms (94.04%), Space: 14.4 MB (77.26%) - LeetHub

---
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,75 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */ 
+
+class Solution {
+public:
+    int mxdep=0;
+    TreeNode* ans=NULL;
+    int howmany=0;
+    
+    void finddist(TreeNode* root,int cd)
+    {  
+        if(cd>mxdep){
+            mxdep=cd; 
+        } 
+        
+        if(!root) return ;
+        
+        finddist(root->left,cd+1);
+        finddist(root->right,cd+1);
+        
+    }
+    
+     void abc(TreeNode* root,int cd)
+    {  
+        if(cd==(mxdep)){  
+            howmany++;   
+        } 
+          
+        if(!root) return ;
+         
+        abc(root->left,cd+1);
+        abc(root->right,cd+1);
+        
+    }
+    
+    int collect(TreeNode* root,int cd)
+    {
+        if(!root)
+        {
+            if(cd==mxdep){
+                return 1;
+            }
+            return 0;
+        }
+        
+        int left=collect(root->left,cd+1);
+        int right=collect(root->right,cd+1);
+        
+        if((left + right)==(howmany)){
+            if(ans==NULL)
+                ans=root; 
+        } 
+        
+        return left+right;
+        
+    }
+    
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        finddist(root,0);  
+        abc(root,0);
+        collect(root,0);
+        
+        return ans;
+        
+    }
+};
