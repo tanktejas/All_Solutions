b994fa4f10f18d52558506cd25a53c7fdd08b848
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-08-30 09:42:21 +0530

    Time: 6 ms (23.35%), Space: 6.9 MB (99.86%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -38,16 +38,6 @@ public:
                 
             }
             
-            for(int i=0;i<n;i++)
-            {
-                for(int j=0;j<n;j++)
-                {
-                    cout<<mat[i][j]<<' ';
-                }
-                cout<<endl;
-            } 
-            
-            cout<<endl;
             
             io++;
             j++;
