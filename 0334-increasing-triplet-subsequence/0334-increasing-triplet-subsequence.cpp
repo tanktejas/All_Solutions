7e307034cdbdce5dfb2185a8c379aa43dd46a9ea
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-10-11 23:06:13 +0530

    Time: 116 ms (58.46%), Space: 80.3 MB (5.67%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int min(long long i,int j)
+    {
+        if(i<j) return i;
+        return j;
+    }
+    
+    bool increasingTriplet(vector<int>& nums) {
+        int n=nums.size();
+        
+        vector<vector<long long>>dp(3,vector<long long>(n,0));
+        
+        dp[0][0]=nums[0];
+        dp[1][0]=1ll*2*INT_MAX;
+        dp[2][0]=1ll*2*INT_MAX;
+        for(int i=1;i<n;i++)
+        {
+            dp[0][i]=min(dp[0][i-1],nums[i]);
+            dp[1][i]=dp[1][i-1];
+            dp[2][i]=dp[2][i-1];
+            
+            if(dp[0][i-1] < nums[i])
+            {
+                dp[1][i]=min(dp[1][i],nums[i]);
+            }
+            
+            if(dp[1][i-1] < nums[i])
+            { 
+                dp[2][i]=min(dp[2][i],nums[i]);
+            }  
+        }   
+    
+        // for(int i=0;i<=2;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        
+       return dp[2][n-1]!=1ll*2*INT_MAX;
+        
+    }
+    
+};
