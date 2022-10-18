2483b70716d60c873e2860eececb75c12152eb76
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-10-18 12:44:13 +0530

    Time: 27 ms (91.09%), Space: 7.4 MB (76.14%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -2,37 +2,37 @@ class Solution {
 public:
     int dp[302][11];
     
-//     int find(int s,int d,int n,vector<int> &jd)
-//     {
-//         if(d==0 and s==n)
-//         {
-//             return 0;
-//         }
+    int find(int s,int d,int n,vector<int> &jd)
+    {
+        if(d==0 and s==n)
+        {
+            return 0;
+        }
         
-//         if(d==0 or s==n)
-//         {   
-//             return -1;
-//         } 
+        if(d==0 or s==n)
+        {   
+            return -1;
+        } 
         
-//         if(dp[s][d]!=-1) return dp[s][d];
+        if(dp[s][d]!=-1) return dp[s][d];
         
-//         int maxi=jd[s];
-//         int ans=INT_MAX;
+        int maxi=jd[s];
+        int ans=INT_MAX;
         
-//         for(int i=s;i<n;i++)
-//         {
-//             maxi=max(maxi,jd[i]);
-//             int pick=find(i+1,d-1,n,jd);
+        for(int i=s;i<n;i++)
+        {
+            maxi=max(maxi,jd[i]);
+            int pick=find(i+1,d-1,n,jd);
             
-//             if(pick != -1 and pick != INT_MAX)
-//             {
-//                 ans=min(ans,maxi + pick);
-//             }
-//         }
+            if(pick != -1 and pick != INT_MAX)
+            {
+                ans=min(ans,maxi + pick);
+            }
+        }
         
-//         return dp[s][d]=ans;
+        return dp[s][d]=ans;
         
-//     }
+    }
     
 //      i -> job (1 base)
 //      j -> day
@@ -46,50 +46,56 @@ public:
 //      else we will not explore that
          
     int minDifficulty(vector<int>& jd, int d) {
-        // memset(dp,INT_MAX,sizeof(dp));
+         
         int n=jd.size();
         
         for(int i=0;i<=301;i++)
         {
             for(int j=0;j<=10;j++)
             {
-                dp[i][j]=INT_MAX;
+                // dp[i][j]=INT_MAX; // if tabulation
+                dp[i][j]=-1; // if memoization
             }
         }
         
-        dp[0][0]=0;
+        //***** Tabulation ******
         
-        for(int i=1;i<=n;i++)
-        {
+//         dp[0][0]=0;
+        
+//         for(int i=1;i<=n;i++)
+//         {
             
-            for(int j=1;j<=d;j++)
-            {  
+//             for(int j=1;j<=d;j++)
+//             {  
                  
-                if(i>=j){
-                 int  maxi=jd[i-1];
+//                 if(i>=j){
+//                  int  maxi=jd[i-1];
                     
-                for(int k=1;k<=i;k++)
-                { 
-                     maxi=max(maxi,jd[i-k]);
-                     int ans=dp[i-k][j-1]; 
-                     if(ans!=INT_MAX)
-                     {
-                         dp[i][j]=min(dp[i][j],ans + maxi);     
-                     }
+//                 for(int k=1;k<=i;k++)
+//                 { 
+//                      maxi=max(maxi,jd[i-k]);
+//                      int ans=dp[i-k][j-1]; 
+//                      if(ans!=INT_MAX)
+//                      {
+//                          dp[i][j]=min(dp[i][j],ans + maxi);     
+//                      }
                     
-                }  
+//                 }  
                     
-                }
-            }
-        }
+//                 }
+//             }
+//         }
         
       
-        return dp[n][d]==INT_MAX ? -1 : dp[n][d];
+        // return dp[n][d]==INT_MAX ? -1 : dp[n][d];
           
-        // int ans = find(0,d,n,jd);
         
-        // if(ans==INT_MAX or ans==-1) return -1;
-        // return ans;
+        //********* Memoization *******
+        
+        int ans = find(0,d,n,jd);
+        
+        if(ans==INT_MAX or ans==-1) return -1;
+        return ans;
         
     }
 };
