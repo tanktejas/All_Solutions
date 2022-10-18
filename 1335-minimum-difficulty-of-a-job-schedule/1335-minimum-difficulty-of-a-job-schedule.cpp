5ea6a2618e7f8455c74c1f29fcccc37d1b364c08
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-10-18 12:41:13 +0530

    Time: 26 ms (91.60%), Space: 7.2 MB (89.52%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,47 +1,95 @@
 class Solution {
 public:
-    int dp[301][11];
+    int dp[302][11];
     
-    int find(int s,int d,int n,vector<int> &jd)
-    {
-        if(d==0 and s==n)
-        {
-            return 0;
-        }
+//     int find(int s,int d,int n,vector<int> &jd)
+//     {
+//         if(d==0 and s==n)
+//         {
+//             return 0;
+//         }
         
-        if(d==0 or s==n)
-        {   
-            return -1;
-        } 
+//         if(d==0 or s==n)
+//         {   
+//             return -1;
+//         } 
         
-        if(dp[s][d]!=-1) return dp[s][d];
+//         if(dp[s][d]!=-1) return dp[s][d];
         
-        int maxi=jd[s];
-        int ans=INT_MAX;
+//         int maxi=jd[s];
+//         int ans=INT_MAX;
         
-        for(int i=s;i<n;i++)
-        {
-            maxi=max(maxi,jd[i]);
-            int pick=find(i+1,d-1,n,jd);
+//         for(int i=s;i<n;i++)
+//         {
+//             maxi=max(maxi,jd[i]);
+//             int pick=find(i+1,d-1,n,jd);
             
-            if(pick != -1 and pick != INT_MAX)
+//             if(pick != -1 and pick != INT_MAX)
+//             {
+//                 ans=min(ans,maxi + pick);
+//             }
+//         }
+        
+//         return dp[s][d]=ans;
+        
+//     }
+    
+//      i -> job (1 base)
+//      j -> day
+     
+//      dp[0][0] = 0;
+//      dp[i][0] = INT_MAX;
+     
+         
+//      i>=j then possible 
+//           dp[i][j]= min(dp[i-k][j-1] for all  k)
+//      else we will not explore that
+         
+    int minDifficulty(vector<int>& jd, int d) {
+        // memset(dp,INT_MAX,sizeof(dp));
+        int n=jd.size();
+        
+        for(int i=0;i<=301;i++)
+        {
+            for(int j=0;j<=10;j++)
             {
-                ans=min(ans,maxi + pick);
+                dp[i][j]=INT_MAX;
             }
         }
         
-        return dp[s][d]=ans;
+        dp[0][0]=0;
         
-    }
-    
-    int minDifficulty(vector<int>& jd, int d) {
-        memset(dp,-1,sizeof(dp));
-        int n=jd.size();
+        for(int i=1;i<=n;i++)
+        {
+            
+            for(int j=1;j<=d;j++)
+            {  
+                 
+                if(i>=j){
+                 int  maxi=jd[i-1];
+                    
+                for(int k=1;k<=i;k++)
+                { 
+                     maxi=max(maxi,jd[i-k]);
+                     int ans=dp[i-k][j-1]; 
+                     if(ans!=INT_MAX)
+                     {
+                         dp[i][j]=min(dp[i][j],ans + maxi);     
+                     }
+                    
+                }  
+                    
+                }
+            }
+        }
         
-        int ans = find(0,d,n,jd);
+      
+        return dp[n][d]==INT_MAX ? -1 : dp[n][d];
+          
+        // int ans = find(0,d,n,jd);
         
-        if(ans==INT_MAX or ans==-1) return -1;
-        return ans;
+        // if(ans==INT_MAX or ans==-1) return -1;
+        // return ans;
         
     }
 };
