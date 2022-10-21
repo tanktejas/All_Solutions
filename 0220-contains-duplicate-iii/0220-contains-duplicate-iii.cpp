a1328f498dafba497317ff40439abc371d2bb312
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-10-21 17:14:22 +0530

    Time: 346 ms (50.88%), Space: 92.2 MB (13.37%) - LeetHub

---
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int id, int vd) {
+        int n=nums.size();
+        
+        
+        vector<pair<int,int>>v;
+        
+        for(int i=0;i<n;i++)
+        {
+            v.push_back(make_pair(nums[i],i));
+        }
+        
+        sort(v.begin(),v.end());
+        
+        int i=0,j=1;
+        
+        set<int>s;
+        s.insert(v[0].second);
+        
+        // for(int i=0;i<n;i++) cout<<v[i].second<<' ';
+          
+        while(j<n)
+        {
+            int curridx=v[j].second;
+            int currval=v[j].first;
+            
+            // cout<<curridx<<" ";
+            
+            while((currval - v[i].first) > vd)
+            { 
+                 // cout<<"delete "<<' ';
+                 s.erase(v[i].second);
+                 i++;
+            }
+            
+            // cout<<s.size()<<"   ";
+             
+            if(!s.empty()){
+            
+            auto kk=s.lower_bound(curridx);
+            
+            int idx=(*kk);
+             
+            // cout<<idx<<"       ";  
+            
+            if(!s.empty() and kk!=s.end() and abs(curridx - idx) <= id and (currval - nums[idx]) <= vd) {
+              cout<<curridx<<" "<<idx; 
+                return 1;
+            }
+            
+            if(kk!=s.begin() and !s.empty())
+            {
+                kk--;  
+                idx=(*kk);
+                
+                if(abs(curridx - idx) <= id and abs(currval - nums[idx]) <= vd){  
+                   cout<<curridx<<" "<<idx;   
+                    return 1;
+                }  
+            }
+            
+            }
+            s.insert(curridx);
+            
+            j++;
+        }
+        
+        return 0;
+    }
+};
