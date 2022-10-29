5395a04e17ca91fd88f7f55a35bbad3c4377d963
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-10-29 10:38:11 +0530

    Time: 631 ms (58.43%), Space: 168.3 MB (62.65%) - LeetHub

---
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int earliestFullBloom(vector<int>&pt, vector<int>& gt) {
+         
+        
+        vector<pair<int,int>>v;
+        int n=gt.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            v.push_back({gt[i],pt[i]});
+        }
+        
+        sort(v.begin(),v.end(),[](pair<int,int> &a,pair<int,int> &b)
+             {
+                 return a.first > b.first;
+             });
+        
+        int maxi=0;
+        
+        int pela=-1;
+        
+        for(int i=0;i<n;i++)
+        { 
+            maxi=max(maxi,pela+v[i].first+v[i].second+1);
+            pela+=(v[i].second);
+        }
+         
+        return maxi;
+    }
+};
