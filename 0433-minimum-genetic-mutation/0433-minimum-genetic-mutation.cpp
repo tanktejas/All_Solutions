33ba2ebfd05d41aa93ade2411bd5a11dc4c26bb8
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-11-02 09:54:40 +0530

    Time: 7 ms (9.25%), Space: 8.2 MB (7.33%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -7,9 +7,7 @@ public:
     
     int find(string s,string e,int mu)
     { 
-       
-        // cout<<s<<" ";
-        
+         
         if(s==e){
             cout<<1;
             f=min(f,mu);
@@ -42,8 +40,42 @@ public:
              mp[bank[i]]++;
          }
         
-        find(start,end,0);
+         queue<string>q;
+         int cnt=0;
+         q.push(start);
         
-        return f==INT_MAX ? -1 : f;
+         while(!q.empty())
+         {
+             
+             int siz=q.size(); 
+             
+             while(siz--)
+             {
+                 string curr=q.front();
+                 q.pop(); 
+                 if(curr==end){ 
+                     return cnt;  
+                 }
+                 
+                 for(int i=0;i<8;i++)
+                 {
+                     
+                     for(int j=0;j<26;j++)
+                     {
+                         string h=curr;
+                         h[i]='A'+j;
+                         if(mp[h] and h!=start){
+                             mp[h]=0;
+                             q.push(h);
+                         }
+                     }
+                 }
+                 
+             }
+             
+            cnt++;
+         }  
+         
+        return -1;
     }
 };
