32e354b26fbb491fdd22fd52a5f899fab22f4032
tanktejas <77108255+tanktejas@users.noreply.github.com>
2022-11-03 19:31:13 +0530

    Time: 42 ms (85.01%), Space: 20.2 MB (45.77%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string longestPrefix(string s) {
+        int n=s.length();
+        
+        vector<int>dp(n+1,0);
+        
+        int i=1,j=0;
+        
+        while(i<n)
+        {   
+           // cout<<i<<' '<<j<<endl;
+           if(s[j]==s[i])
+            { 
+                j++;
+                dp[i]=j;
+                i++;
+            }else if(j!=0)
+            { 
+               if(j!=0)
+                  j=dp[j-1]; 
+                
+            }else if(j==0){
+               i++;
+           }
+            
+          
+            
+        }
+         
+        return s.substr(0,dp[n-1]); 
+    }
+};
